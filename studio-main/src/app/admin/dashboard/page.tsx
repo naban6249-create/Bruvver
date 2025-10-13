ad3385d93467cc0c68e98e442f43b4537c580eb2
naban6249-create <naban6249@gmail.com>
2025-10-13 17:25:21 +0530

    Update page.tsx

--- studio-main/src/app/admin/dashboard/page.tsx
+++ studio-main/src/app/admin/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { DashboardClient } from "../../../components/admin/dashboard-client";
 
+export const dynamic = 'force-dynamic';
 export default function DashboardPage() {
     return (
         <Suspense fallback={<div>Loading dashboard...</div>}>
